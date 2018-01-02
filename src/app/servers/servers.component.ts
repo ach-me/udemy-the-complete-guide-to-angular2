b242da2b6b75ea46410efbbce3a5ba77fd6812f2
ach <ignaciomenchaca@gmail.com>
2018-01-02 03:01:14 -0300

    Se agregó la directiva *ngIf al servers.component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
 
   // Metodo ejecutado al momento en que este componente es creado por Angular
   constructor() {    
@@ -24,6 +25,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
